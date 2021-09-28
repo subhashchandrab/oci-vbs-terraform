a740b186b2ba09bb355b38a1823f44a1db462766
subhashchandrab <subhashchandra.b@gmail.com>
2021-09-28 21:53:13 +0530

    Update run_terraform.sh

--- build/run_terraform.sh
+++ build/run_terraform.sh
@@ -1,13 +1,13 @@
-echo $(pwd)
+
 # Update the environment variables
 source terraform/env_vars
 
-#Copy the public and private key files to terraform directory
-cp oci_api_key_private.pem ~/.terraform.d/
-cp oci_api_key_pub.pem ~/.terraform.d/
+# Copy the public and private key files to terraform directory
+cp oci-keys/oci_api_key_private.pem ~/.terraform.d/
+cp oci-keys/oci_api_key_pub.pem ~/.terraform.d/
 
 
-#Generate the public/private key pair on the current VM which can be used to connect to the compute instance provisioned using terraform
+# Generate the public/private key pair on the current VM which can be used to connect to the compute instance provisioned using terraform
 ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y
 
 terraform --version
