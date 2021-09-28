92057e232eb6c8058dfa41e4ed9644075f6870bf
subhashchandrab <subhashchandra.b@gmail.com>
2021-09-28 19:04:41 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,38 @@
+# Initialize the local values to be used in the script
 locals {
   all_ip_cidr_wildcard = "0.0.0.0/0" // CIDR block to wildcard all ip addresses
   tcp_protocol = "6" // Numeric code for TCP protocol
   icmp_protocol = "1"
 }
+ 
+# Create the compartment under the root  
+resource "oci_identity_compartment" "vbs_tf_compartment" {
+    compartment_id = var.tenancy_ocid
+    description = "Compartment provisioned by terraform"
+    name = var.compartment_display_name
+}  
 
+# Initialize the availability domain to be used
 data "oci_identity_availability_domain" "ad" {
   compartment_id = var.tenancy_ocid
   ad_number      = 1
 }
 
+# Create the VCN
 resource "oci_core_vcn" "vbs_tf_vcn" {
   cidr_block     = var.vcn_cidr_block
   compartment_id = var.compartment_ocid
   display_name   = var.vcn_display_name
 }
 
+# Create Internet Gateway
 resource "oci_core_internet_gateway" "vbs_tf_igw" {
   compartment_id = var.compartment_ocid
   display_name   = var.igw_display_name
   vcn_id         = oci_core_vcn.vbs_tf_vcn.id
 }
 
+# Create default route table  for the VCN  with a routing rule to use internet gateway
 resource "oci_core_default_route_table" "default_route_table" {
   manage_default_resource_id = oci_core_vcn.vbs_tf_vcn.default_route_table_id
   display_name               = var.default_route_table_name
@@ -32,6 +44,8 @@ resource "oci_core_default_route_table" "default_route_table" {
   }
 }
 
+
+# Create default security list for the VCN with required ingress and egress rules
 resource "oci_core_default_security_list" "default_security_list" {
   manage_default_resource_id = oci_core_vcn.vbs_tf_vcn.default_security_list_id
   display_name               = var.default_security_list_name
@@ -78,6 +92,7 @@ resource "oci_core_default_security_list" "default_security_list" {
   }
 }
 
+# Create the public subnet under the VCN
 resource "oci_core_subnet" "vbs_tf_subnet" {
   availability_domain = data.oci_identity_availability_domain.ad.name
   cidr_block          = var.public_subnet_cidr
@@ -89,7 +104,7 @@ resource "oci_core_subnet" "vbs_tf_subnet" {
   dhcp_options_id     = oci_core_vcn.vbs_tf_vcn.default_dhcp_options_id
 }
 
-# This module will create a instance based on the shape provided
+# Create the compute instance using the provided shape and images
 module "vbs_tf_compute_instance" {
   source = "oracle-terraform-modules/compute-instance/oci"
   
@@ -107,6 +122,7 @@ output "instance_public_ip" {
   value       = module.vbs_tf_compute_instance.public_ip[0]
 }
 
+# Install httpd(Apache web server) on the provisioned instance using the remote-exec
 resource "null_resource" "remote-exec" {
 
   provisioner "remote-exec" {
@@ -125,7 +141,7 @@ resource "null_resource" "remote-exec" {
         "sudo firewall-cmd --permanent --add-service=http",
         "sudo firewall-cmd --reload",
         "sudo systemctl start httpd",
-       "sudo systemctl enable httpd"
+        "sudo systemctl enable httpd"
     ]
   }
 }
